1d4f01229fa201d608fd56e4365c8a47587f74cc
HPHAN1 <huyphan@rocketmortgage.com>
2022-01-24 09:02:30 -0500

    initial commit

---
+++ src/app/Country.js
@@ -0,0 +1,38 @@
+"use strict";
+exports.__esModule = true;
+exports.Country = void 0;
+var Country = /** @class */ (function () {
+    function Country(countryname, lang, greeting, colors, url) {
+        this.countryname = countryname;
+        this.lang = lang;
+        this.greeting = greeting;
+        this.colors = colors;
+        this.url = url;
+    }
+    return Country;
+}());
+exports.Country = Country;
+var Countries = [];
+var usa = Countries.push(new Country("USA", "English", "Hello World", ["red", "blue", "white"], "USAFlag.png"));
+var egypt = Countries.push(new Country("Egypt", "Arabic", "Marhaban Bialealam", ["red", "black", "white"], "EgyptFlag.png"));
+var france = Countries.push(new Country("France", "French", "Bonjour Le Monde", ["blue", "red", "white"], "FranceFlag.png"));
+var italy = Countries.push(new Country("Italy", "Italian", "Ciao Mondo", ["green", "red", "white"], "ItalyFlag.png"));
+var germany = Countries.push(new Country("Germany", "German", "Hallo Welt", ["black", "yellow", "red"], "GermanyFlag.png"));
+var india = Countries.push(new Country("India", "Hindi", "Namaste Duniya", ["orange", "green", "white"], "IndiaFlag.png"));
+console.log(Countries);
+function SwitchCountry() {
+    var input = document.getElementById("CountryList").value;
+    for (var i = 0; i < Countries.length; i++) {
+        //country = Countries[i];
+        if (input === Countries[i].countryname) {
+            document.getElementById("CountryName").innerText = Countries[i].countryname;
+            document.getElementById("OfficialLanguage").innerText = Countries[i].lang;
+            document.getElementById("HelloWorld").innerText = Countries[i].greeting;
+            document.getElementById("Image").src = Countries[i].url;
+            document.getElementById("Color1").style.color = Countries[i].colors[0];
+            document.getElementById("Color2").style.backgroundColor = Countries[i].colors[1];
+            document.getElementById("Color3").style.backgroundColor = Countries[i].colors[2];
+            break;
+        }
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from './Country';
 
 @Component({
   selector: 'app-root',
